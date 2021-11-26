69da67c8bf03c9160c01d8b1dbd05c3aaf28cd73
cahuc <cahuc@DESKTOP-HCUCG4H>
2021-11-25 19:21:38 -0500

    Heroku deploy

--- src/demo/cards/cards.controller.ts
+++ src/demo/cards/cards.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Req } from '@nestjs/common';
 @Controller('cards')
 export class CardsController {
 
-    getIndex(@Req() request: Request):string{
+    getIndex(@Req() request):string{
 
         return "This are my cards!";
     }
